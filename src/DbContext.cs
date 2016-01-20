4c795f94983c9a31977a6b2389f9c4fb1502700d
Lukasz Krzyszczak <lkrzyszczak@divante.pl>
2016-01-20 13:56:48 +0100

    Add context class

---
+++ src/DbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace CurrencyRates
+{
+    class Context : DbContext
+    {
+        public Context() : base() {}
+
+        public DbSet<Currency> Currencies { get; set; }
+        public DbSet<File> Files { get; set; }
+        public DbSet<Rate> Rates { get; set; }
+    }
+}
