b603bbb894f0e4edd7a4b42c9e9a0dd2c8cfa9a2
Lukasz Krzyszczak <lkrzyszczak@divante.pl>
2016-01-20 17:23:28 +0100

    Add skeleton code to main program

--- src/CurrencyRates/Program.cs
+++ src/CurrencyRates/Program.cs
@@ -1,12 +1,38 @@
-﻿using CurrencyRates.Entity;
-using System;
+﻿using System;
 
 namespace CurrencyRates
 {
     class Program
     {
+        enum Actions { Fetch, Show };
+
         static void Main(string[] args)
-        {    
+        {
+            try
+            {
+                var action = Actions.Fetch;
+
+                if (args.Length > 0)
+                {
+                    action = (Actions)Enum.Parse(typeof(Actions), args[0], true);
+                }
+
+                switch (action)
+                {
+                    case Actions.Fetch:
+                        //@todo implement Fetch action
+                        break;
+
+                    case Actions.Show:
+                        //@todo implement Show action
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (Exception e) {
+                Console.WriteLine("An error occured: " + e.Message);
+            }
         }
     }
 }
