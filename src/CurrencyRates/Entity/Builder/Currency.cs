568248d3394cb2c74cf5683fc521c3ddf566ddb1
Łukasz Krzyszczak <lukasz.krzyszczak@gmail.com>
2016-01-22 17:15:41 +0100

    Add skeleton entity builders

---
+++ src/CurrencyRates/Entity/Builder/Currency.cs
@@ -0,0 +1,11 @@
+﻿namespace CurrencyRates.Entity.Builder
+{
+    class Currency
+    {
+        public Entity.Currency BuildFromNbpXml(string xml)
+        {
+            //@todo implement method
+            return new Entity.Currency();
+        }
+    }
+}
---
+++ src/CurrencyRates/Entity/Builder/Rate.cs
@@ -0,0 +1,11 @@
+﻿namespace CurrencyRates.Entity.Builder
+{
+    class Rate
+    {
+        public Entity.Rate BuildFromNbpXml(string xml)
+        {
+            //@todo implement method
+            return new Entity.Rate();
+        }
+    }
+}
