e522428628b16fc4c44541f70e7221033720930b
Łukasz Krzyszczak <lukasz.krzyszczak@gmail.com>
2016-01-28 17:36:14 +0100

    Fix coding style issue

--- Migrations/Configuration.cs
+++ Migrations/Configuration.cs
@@ -2,7 +2,7 @@ namespace CurrencyRates.Migrations
 {
     using System.Data.Entity.Migrations;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<CurrencyRates.Context>
+    internal sealed class Configuration : DbMigrationsConfiguration<Context>
     {
         public Configuration()
         {
