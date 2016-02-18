01c4deea636d30a8cfc89e6c0879907be9987aac
Łukasz Krzyszczak <lukasz.krzyszczak@gmail.com>
2016-02-18 15:43:11 +0100

    Add web application skeleton

---
+++ CurrencyRates.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace CurrencyRates.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }      
+    }
+}
---
+++ CurrencyRates.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CurrencyRates.Web.MvcApplication" Language="C#" %>
