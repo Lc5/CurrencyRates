751381401a1470435937b419e2dd681d78eaf595
Łukasz Krzyszczak <lukasz.krzyszczak@gmail.com>
2016-03-25 11:36:57 +0100

    Make interval configurable in Scheduler

--- CurrencyRates.WindowsService/Scheduler.cs
+++ CurrencyRates.WindowsService/Scheduler.cs
@@ -8,26 +8,28 @@ namespace CurrencyRates.WindowsService
     public partial class Scheduler : ServiceBase
     {
         static int EventId;
+        readonly int Interval;
         readonly Synchronizer Synchronizer;
 
-        public Scheduler(Synchronizer synchronizer)
+        public Scheduler(Synchronizer synchronizer, int interval = 60000)
         {
             Synchronizer = synchronizer;
+            Interval = interval;
             InitializeComponent();
         }
 
         protected override void OnStart(string[] args)
         {
-            EventLog.WriteEntry(GetType().Name + " started.", EventLogEntryType.Information, ++EventId);
+            EventLog.WriteEntry(GetType().FullName + " started.", EventLogEntryType.Information, ++EventId);
 
-            var timer = new Timer() { Interval = 60000 };
+            var timer = new Timer() { Interval = Interval };
             timer.Elapsed += OnTimer;
             timer.Start();
         }
 
         protected override void OnStop()
         {
-            EventLog.WriteEntry(GetType().Name + " stopped.", EventLogEntryType.Information, ++EventId);
+            EventLog.WriteEntry(GetType().FullName + " stopped.", EventLogEntryType.Information, ++EventId);
         }
 
         void OnTimer(object sender, ElapsedEventArgs args)
