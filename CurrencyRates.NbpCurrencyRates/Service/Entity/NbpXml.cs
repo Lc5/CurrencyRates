c8a23379823fd6bbeea4274f37ac455e97b44904
Łukasz Krzyszczak <lukasz.krzyszczak@gmail.com>
2016-04-04 13:12:47 +0200

    Fix CS

--- CurrencyRates.NbpCurrencyRates/Service/Entity/NbpXml.cs
+++ CurrencyRates.NbpCurrencyRates/Service/Entity/NbpXml.cs
@@ -6,8 +6,8 @@
         public const string CurrencyCode = "kod_waluty";
         public const string CurrencyName = "nazwa_waluty";
         public const string Multiplier = "przelicznik";
-        public const string PublicationDate = "data_publikacji";
         public const string Position = "pozycja";
+        public const string PublicationDate = "data_publikacji";
         public const string RateTable = "tabela_kursow";
         public const string TableNumber = "numer_tabeli";
         public const string Type = "typ";
