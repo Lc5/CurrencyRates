4c16346a3a08af65f0e0d5bdbcf1903f755673b3
Łukasz Krzyszczak <lukasz.krzyszczak@gmail.com>
2016-04-15 16:39:46 +0200

    Add tests for WebServiceInstaller

---
+++ CurrencyRates.Tests/Web/Installers/WebServiceInstallerTest.cs
@@ -0,0 +1,34 @@
+﻿namespace CurrencyRates.Tests.Web.Installers
+{
+    using Castle.Facilities.WcfIntegration;
+    using Castle.Windsor;
+
+    using CurrencyRates.Tests.TestUtils;
+    using CurrencyRates.Web.Installers;
+
+    using NUnit.Framework;
+
+    using ICurrencyRatesService = CurrencyRates.WebService.ICurrencyRatesService;
+    using ICurrencyRatesServiceClient = CurrencyRates.Web.CurrencyRatesService.ICurrencyRatesService;
+
+    [TestFixture]
+    public class WebServiceInstallerTest
+    {
+        private IWindsorContainer containerWithWebservice;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.containerWithWebservice = new WindsorContainer();
+            this.containerWithWebservice.Install(new WebServiceInstaller());
+        }
+
+        [Test]
+        public void TestWebserviceIsRegistered()
+        {
+            this.containerWithWebservice.Resolve<WcfServiceExtension>();
+            Assert.That(this.containerWithWebservice.GetHandlersFor(typeof(ICurrencyRatesService)), Is.Not.Empty);
+            Assert.That(this.containerWithWebservice.GetHandlersFor(typeof(ICurrencyRatesServiceClient)), Is.Not.Empty);          
+        }
+    }
+}
--- CurrencyRates.Web/Installers/WebServiceInstaller.cs
+++ CurrencyRates.Web/Installers/WebServiceInstaller.cs
@@ -5,15 +5,28 @@
     using Castle.MicroKernel.SubSystems.Configuration;
     using Castle.Windsor;
 
-    using CurrencyRates.Web.CurrencyRatesService;
+    using CurrencyRates.Web.CurrencyRatesService;    
+    using CurrencyRates.WebService;
+
+    using ICurrencyRatesService = CurrencyRates.WebService.ICurrencyRatesService;
+    using ICurrencyRatesServiceClient = CurrencyRates.Web.CurrencyRatesService.ICurrencyRatesService;
 
     public class WebServiceInstaller : IWindsorInstaller
     {
         public void Install(IWindsorContainer container, IConfigurationStore store)
         {
             container.AddFacility<WcfFacility>();
-            container.Register(Component.For<CurrencyRates.WebService.ICurrencyRatesService>().ImplementedBy<CurrencyRates.WebService.CurrencyRatesService>().AsWcfService());
-            container.Register(Component.For<ICurrencyRatesService>().ImplementedBy<CurrencyRatesServiceClient>());
+
+            container.Register(
+                Component
+                    .For<ICurrencyRatesService>()
+                    .ImplementedBy<CurrencyRatesService>()
+                    .AsWcfService());
+
+            container.Register(
+                Component
+                    .For<ICurrencyRatesServiceClient>()
+                    .ImplementedBy<CurrencyRatesServiceClient>());
         }
     }
 }
